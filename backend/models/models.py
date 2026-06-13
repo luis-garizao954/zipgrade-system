dffe1b6e2f3d1f313d7f9a107fa4e195c9c60989
luis-garizao954 <legp1925a@gmail.com>
2026-06-12 21:20:16 -0500

    modelo Resultado con nombre_temp

--- backend/models/models.py
+++ backend/models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Boolean, DateTime, Integer, Numeric, Date, ForeignKey, BigInteger, Text
+from sqlalchemy import Column, Boolean, DateTime, Integer, Numeric, Date, ForeignKey, BigInteger, Text
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy.sql import func
